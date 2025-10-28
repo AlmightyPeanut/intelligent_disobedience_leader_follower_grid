4bd251387f4a59c391f45fedc05ae1786d86b62b
AlmightyPeanut <benedikthornig@outlook.com>
2025-10-28 17:35:47 -0600

    add leader/follower only observation and action spaces to LavaEnv move one hot encoding directly into the observations of LavaEnv LavaEnv reward encoding/decoding

--- minigrid_env/environment.py
+++ minigrid_env/environment.py
@@ -1,14 +1,17 @@
+from __future__ import annotations
+
 from enum import IntEnum
+from typing import SupportsFloat, Any
 
 import numpy as np
-import torch as th
 from gymnasium import spaces
+from gymnasium.core import ActType, ObsType
+from minigrid.core.constants import OBJECT_TO_IDX
 from minigrid.core.grid import Grid
 from minigrid.core.mission import MissionSpace
 from minigrid.core.world_object import Goal, WorldObj, Lava
 from minigrid.manual_control import ManualControl
 from minigrid.minigrid_env import MiniGridEnv
-from stable_baselines3.common.type_aliases import PyTorchObs
 
 
 class Actions(IntEnum):
@@ -17,6 +20,26 @@ class Actions(IntEnum):
     right = 1
     forward = 2
     pickup = 3  # represents the null action if the control says it is dangerous
+
+
+class LeaderActions(IntEnum):
+    left = 0
+    right = 1
+    forward = 2
+
+
+class FollowerActions(IntEnum):
+    obey = 0
+    disobey = 1
+
+
+class RewardEncoding(IntEnum):
+    leader_positive = 1
+    leader_neutral = 1 << 1
+    leader_negative = 1 << 2
+    follower_positive = 1 << 3
+    follower_neutral = 1 << 4
+    follower_negative = 1 << 5
 
 
 class Agent(WorldObj):
@@ -35,10 +58,20 @@ class LavaEnv(MiniGridEnv):
             agent_start_direction=0,
             max_steps: int | None = None,
             lava_tile_percentage: float = 0.1,
+            only_leader_env: bool = False,
+            only_follower_env: bool = False,
+            one_hot_encode_tiles: bool = True,
+            channel_first_obs: bool = True,
             **kwargs,
     ):
         self.agent_start_position = agent_start_position
         self.agent_start_direction = agent_start_direction
+        self.only_leader_env = only_leader_env
+        self.only_follower_env = only_follower_env
+        self.channel_first_obs = channel_first_obs
+
+        self.one_hot_encode_tiles = one_hot_encode_tiles
+        self._one_hot_encoding = np.eye(len(OBJECT_TO_IDX), dtype=np.uint8)
 
         mission_space = MissionSpace(mission_func=self._generate_mission)
 
@@ -54,12 +87,76 @@ class LavaEnv(MiniGridEnv):
             **kwargs,
         )
 
-        self.actions = Actions
-        self.action_space = spaces.Discrete(len(self.actions))
+        assert isinstance(self.observation_space, spaces.Dict)
+        self.observation_space = spaces.Dict({
+            "image": self.observation_space.spaces["image"],
+        })
+
+        assert isinstance(self.observation_space, spaces.Dict)
+        follower_observation_space = spaces.Box(
+            0, 255,
+            (grid_size, grid_size, len(LeaderActions) + self.observation_space.spaces["image"].shape[-1]),
+            dtype=np.uint8)
+        self.observation_space.spaces["follower_image"] = follower_observation_space
+
+        if self.one_hot_encode_tiles:
+            self.observation_space["image"] = spaces.Box(
+                0, 1,
+                (
+                    self.observation_space.spaces["image"].shape[0],
+                    self.observation_space.spaces["image"].shape[1],
+                    len(OBJECT_TO_IDX),
+                )
+            )
+
+            self.observation_space["follower_image"] = spaces.Box(
+                0, 1,
+                (
+                    self.observation_space.spaces["image"].shape[0],
+                    self.observation_space.spaces["image"].shape[1],
+                    len(LeaderActions) + len(OBJECT_TO_IDX),
+                )
+            )
+
+        if self.channel_first_obs:
+            self.observation_space["image"] = spaces.Box(
+                0, 1 if self.one_hot_encode_tiles else 255,
+                (
+                    self.observation_space.spaces["image"].shape[-1],
+                    grid_size,
+                    grid_size,
+                )
+            )
+
+            self.observation_space["follower_image"] = spaces.Box(
+                0, 1 if self.one_hot_encode_tiles else 255,
+                (
+                    self.observation_space.spaces["follower_image"].shape[-1],
+                    grid_size,
+                    grid_size,
+                )
+            )
+
+
+
+        self.leader_actions = LeaderActions
+        self.leader_action_space = spaces.Discrete(len(self.leader_actions))
+        self.follower_actions = FollowerActions
+        self.follower_action_space = spaces.Discrete(len(self.follower_actions))
+        self.action_space = spaces.MultiDiscrete([len(self.leader_actions), len(self.follower_actions)])
+
+        if self.only_leader_env:
+            self.observation_space = self.observation_space.spaces["image"]
+            self.action_space = self.leader_action_space
+        elif self.only_follower_env:
+            self.observation_space = self.observation_space.spaces["follower_image"]
+            self.action_space = self.follower_action_space
+
+        self.lava_tile = Lava
 
     @staticmethod
     def _generate_mission():
-        return "Lava environment"
+        return "Reach the goal tile without going through lava tiles"
 
     def _gen_grid(self, width, height):
         self.grid = Grid(width, height)
@@ -75,8 +172,115 @@ class LavaEnv(MiniGridEnv):
 
         lava_tiles = int((self.grid.width - 2) * (self.grid.height - 2) * self.lava_tile_percentage)
         for _ in range(lava_tiles):
-            self.place_obj(Lava(), top=(1, 1), size=(self.grid.width - 2, self.grid.height - 2))
+            self.place_obj(self.lava_tile(), top=(1, 1), size=(self.grid.width - 2, self.grid.height - 2))
         # todo: check if there is a possible path from start to finish
+
+    def step(
+            self, action: ActType
+    ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        combined_action = self.combine_actions(action)
+        next_obs, leader_reward, terminated, truncated, info = super().step(combined_action)
+
+        # if the agent stepped into lava, the reward for the follower is -1
+        follower_reward = -1 if isinstance(self.grid.get(*self.agent_pos), Lava) else 1
+
+        # reward needs to be encoded to be compatible with gymnasium
+        # TODO: test if correct rewards are given
+        encoded_reward = self.encode_reward(leader_reward, follower_reward)
+        assert self.decode_reward(encoded_reward) == (leader_reward, follower_reward)
+
+        return next_obs, encoded_reward, terminated, truncated, info
+
+    @staticmethod
+    def combine_actions(
+            action: np.ndarray,
+    ):
+        combined_action = np.where(action[1] == FollowerActions.obey, action[0], Actions.pickup)
+        return combined_action.reshape((1,))
+
+    @staticmethod
+    def encode_reward(leader_reward: SupportsFloat, follower_reward: SupportsFloat) -> int:
+        encoded_reward = 0
+
+        leader_reward = float(leader_reward)
+        if leader_reward > 0:
+            encoded_reward |= RewardEncoding.leader_positive
+        elif leader_reward < 0:
+            encoded_reward |= RewardEncoding.leader_negative
+        else:
+            encoded_reward |= RewardEncoding.leader_neutral
+
+        follower_reward = float(follower_reward)
+        if follower_reward > 0:
+            encoded_reward |= RewardEncoding.follower_positive
+        elif follower_reward < 0:
+            encoded_reward |= RewardEncoding.follower_negative
+        else:
+            raise NotImplementedError
+
+        return encoded_reward
+
+    @staticmethod
+    def decode_reward(encoded_reward: int) -> tuple[SupportsFloat, SupportsFloat]:
+        if type(encoded_reward) is not int:
+            encoded_reward = int(encoded_reward)
+        if encoded_reward & RewardEncoding.leader_positive:
+            leader_reward = 1
+        elif encoded_reward & RewardEncoding.leader_negative:
+            leader_reward = -1
+        else:
+            leader_reward = 0
+
+        if encoded_reward & RewardEncoding.follower_positive:
+            follower_reward = 1
+        elif encoded_reward & RewardEncoding.follower_negative:
+            follower_reward = -1
+        else:
+            raise NotImplementedError
+
+        return leader_reward, follower_reward
+
+    def gen_obs(self):
+        obs = super().gen_obs()
+
+        if self.one_hot_encode_tiles:
+            obs["image"] = self._one_hot_encoding[obs["image"][..., 0]]
+
+        # give obs without leader action here, because there the leader hasn't decided its action yet
+        follower_obs = np.concatenate([
+            obs["image"],
+            np.zeros((obs["image"].shape[0], obs["image"].shape[1], self.leader_action_space.n), dtype=np.uint8),
+        ], axis=-1)
+        obs["follower_image"] = follower_obs
+
+        # hide lava tiles from the leader
+        lava_tile_encoding = self.lava_tile().encode()
+        obs["image"][..., :] = np.where(
+            obs["image"][..., :] == (lava_tile_encoding if not self.one_hot_encode_tiles else OBJECT_TO_IDX["lava"]),
+            np.array([OBJECT_TO_IDX["empty"], 0, 0]) if not self.one_hot_encode_tiles else OBJECT_TO_IDX["empty"],
+            obs["image"][..., :]
+        )
+
+        if self.channel_first_obs:
+            obs["image"] = np.moveaxis(obs["image"], -1, 0)
+            obs["follower_image"] = np.moveaxis(obs["follower_image"], -1, 0)
+
+        if self.only_leader_env:
+            return obs["image"]
+        elif self.only_follower_env:
+            return obs["follower_image"]
+        return {
+            "image": obs["image"],
+            "follower_image": obs["follower_image"],
+        }
+
+    def prepare_follower_obs(self, obs: np.ndarray, leader_action: np.ndarray = None):
+        # Overwrites the action in the observation
+        assert obs.shape == self.observation_space.spaces["follower_image"].shape
+        obs[..., -self.leader_action_space.n:] = 0
+        if leader_action is not None:
+            obs[..., leader_action] = 1
+        return obs
 
     # def gen_obs_grid(self, agent_view_size=None):
     #     # agent has perfect information
@@ -94,16 +298,6 @@ class LavaEnv(MiniGridEnv):
     #     )
     #
     #     return img
-    @classmethod
-    def prepare_follower_obs(cls, obs: PyTorchObs, leader_action: np.ndarray):
-        leader_action_obs = th.zeros((obs.shape[0],
-                                      len(Actions),
-                                      obs.shape[2],
-                                      obs.shape[3]), )
-        leader_action_obs[:, leader_action, ...] = 1
-        follower_obs = th.cat([obs, leader_action_obs], dim=1)
-
-        return follower_obs
 
 
 def main():
